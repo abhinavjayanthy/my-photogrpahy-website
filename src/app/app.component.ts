5145f14b1fb6308bbdaa3b2bba77a85cf4afaf47
Prabhala, Bharadwaj <bharadwaj.prabhala@one.verizon.com>
2017-08-19 17:05:40 -0400

    Photos - Side Navbar, Thumbnails and Modal Popups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  loadedFeature = 'home';
+export class AppComponent implements OnInit, OnChanges {
+  @Input() loadedFeature: string;
+
+  ngOnInit(){
+      this.loadedFeature = 'home';
+  }
+
+  ngOnChanges(){
+
+  }
+
+
   onNavigate(feature :string){
     this.loadedFeature = feature
+  }
+
+  isSection(section: string){
+      return this.loadedFeature === section;
   }
 }
---
+++ src/app/photos/thumbnail/thumbnail.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-thumbnail',
+  templateUrl: './thumbnail.component.html',
+  styleUrls: ['./thumbnail.component.css']
+})
+export class ThumbnailComponent implements OnInit, OnChanges {
+
+  @Input() imgLoc: string;
+  @Input() imgType: string;
+  @Input() imgName: string;
+  @Output() clickedImg = new EventEmitter();
+  
+  imgLocModal: string;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngOnChanges(){
+     
+  }
+
+
+  onClickImg(imgLoc: string){
+    this.clickedImg.emit({imgLocModal: imgLoc});
+  }
+
+}
